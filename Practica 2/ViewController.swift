c6620c2a2297cca2d5e034bd5ea5ab481d6083bc
Alumno <ulsa@d105.local>
2021-08-25 16:08:42 -0700

    Initial Commit

---
+++ Practica 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practica 2
+//
+//  Created by Alumno on 25/08/21.
+//  Copyright © 2021 Ruben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
